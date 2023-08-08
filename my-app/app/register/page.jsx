2670b863be624418633d3bd99ac428536f11877d
nazlicansahin <76625343+nazlicansahin@users.noreply.github.com>
2023-08-08 13:32:58 +0300

    Create Form
    Create login form- only frontend
    Create register form - only front-end
    Create app document for routing

---
+++ my-app/app/register/page.jsx
@@ -0,0 +1,7 @@
+import RegisterForm from "@/components/RegisterForm"
+
+export default function Register() { //register linki bu dosyaya yönlendiriyor
+    return (<RegisterForm />)
+
+}
+
---
+++ my-app/components/RegisterForm.jsx
@@ -0,0 +1,5 @@
+export default function RegisterForm() {// RegisterForm u register app icindfeki register page e y,nlendiriyoruz
+    return (<div className="grid place-items-center h-screen" >Register</div>)
+
+
+}
