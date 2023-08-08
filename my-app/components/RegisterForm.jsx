97de24a6db09a57a68af103bdfcd0bed85d0c7d1
nazlicansahin <76625343+nazlicansahin@users.noreply.github.com>
2023-08-08 14:49:21 +0300

    Update RegisterForm.jsx
    Design useState fields
    create api folder

--- my-app/components/RegisterForm.jsx
+++ my-app/components/RegisterForm.jsx
@@ -1,18 +1,34 @@
+'use client'
+
 import Link from "next/link"
+import { useState } from "react"
 export default function RegisterForm() {// RegisterForm u register app icindfeki register page e y,nlendiriyoruz
+    const [name, setName] = useState('')
+    const [nickname, setNickname] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [error, setError] = useState('')
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!name || !email || !nickname || !password) {
+            setError('All fields are necessary')
+        }
+    }
     return (
         <div className="grid place-items-center h-screen">
 
             <div className="shadow-lg p-5 rounded-lg border-t-4 border-pink-400">
                 <h1 className="text-xl font-bold my-4">Enter the details</h1>
-                <form className="flex flex-col gap-3">
-                    <input type="text" placeholder=" Enter your full name" className="block border border-grey-light w-full p-3 rounded mb-4" name="name" />
-                    <input type="text" placeholder="Enter your nickname" className="block border border-grey-light w-full p-3 rounded mb-4" name="name" />
-                    <input type="text" placeholder="Enter your email" className="block border border-grey-light w-full p-3 rounded mb-4" name="name" />
-                    <input type="password" placeholder="Enter your password" className="block border border-grey-light w-full p-3 rounded mb-4" name="name" />
+                <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+                    <input onChange={(e) => setName(e.target.value)} value={name} type="text" placeholder=" Enter your full name" className="block border border-grey-light w-full p-3 rounded mb-4" name="name" />
+                    <input onChange={(e) => setNickname(e.target.value)} value={nickname} type="text" placeholder="Enter your nickname" className="block border border-grey-light w-full p-3 rounded mb-4" name="name" />
+                    <input onChange={(e) => setEmail(e.target.value)} value={email} type="text" placeholder="Enter your email" className="block border border-grey-light w-full p-3 rounded mb-4" name="name" />
+                    <input onChange={(e) => setPassword(e.target.value)} value={password} type="password" placeholder="Enter your password" className="block border border-grey-light w-full p-3 rounded mb-4" name="name" />
                     <button className="w-full text-center py-3 rounded bg-pink-500 text-white hover:bg-pink-700 focus:outline-none my-1">Register</button>
-                    <div className="  bg-slate-500 text-sm text-white mt-2 w-fit px-3 py-1 rounded-sm">Error message</div>
-                    <Link href={"/register"} className='text-sm mt-3 text-right px-5'>
+
+                    {error && (
+                        <div className="  bg-slate-500 text-sm text-white mt-2 w-fit px-3 py-1 rounded-sm">{error}</div>
+                    )}<Link href={"/register"} className='text-sm mt-3 text-right px-5'>
                         Already have an account? <span className='underline'>Login here</span>
                     </Link>
                 </form>
